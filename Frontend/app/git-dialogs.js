83586cd18c3086e28847cbaedfc27cd8a000d63c
DIRM2705 <dirm2705@gmail.com>
2025-01-10 16:18:50 -0600

    Add GUI for URL

---
+++ Frontend/app/git-dialogs.js
@@ -0,0 +1,7 @@
+const { invoke } = window.__TAURI__.tauri;
+const { listen } = window.__TAURI__.event;
+
+function sendURL()
+{
+    
+}
--- src-tauri/src/commands/git_cmds.rs
+++ src-tauri/src/commands/git_cmds.rs
@@ -5,7 +5,7 @@ pub fn create_repo(url : String, path : String) -> Result<(), String> {
     let mut runner = Runner::new(&path);
 
     runner.exec_cmd("git init")?;
-    //runer.exec_cmd("git remote add origin", vec![url]))?;
+    runner.exec_cmd(&format!("git remote add origin {}", url))?;
     runner.exec_cmd("git branch -M main")?;
 
     return Ok(());
